5b0d703712088331209184c38c1d572e41a2315c
VEfimov <V.Efimov**.ru>
2020-09-11 15:45:52 +0300

    new branch final, delete other Classes, looks good in one main class

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
-/*
-Для перевода из 16 в 10 нужно:
-каждая 10 часть - это 16 в степени, начиная с 0. 921 = 1*16^0= 1; 2*16^1=2*16; 9*16^2 = 9*256;
-потом все эти числа складываются между собой ( 1 + 32 + 2304 )
- */
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Введите данные из строки ответ через пробел ( )");
-        GetString String = new GetString();
-        String answer = String.answer;
-        GetFRAnswer FRAnswer = new GetFRAnswer();
-        CalculateDecimal resultDecimal = new CalculateDecimal();
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();         // 55 00 15 07 02 1D 01 - 15 - 21 год
+//        String answer = "55 00 15 07 1F 1D 01";       // просто строка для проверки, что бы не вводить все время заново.
 
-        System.out.println("\tВы ввели: " + answer);                                                              //для визуальной проверки ввода
-        System.out.println("\t" + FRAnswer.readFRanswer() + " \n\tФН годен до: (дд/мм/гг) \n\t\t" + resultDecimal.SwitchDay() + " : " + resultDecimal.SwitchMounth() + " : 20" + resultDecimal.SwitchYear());
+//проверка на успешное чтение. 55 00 - значит, что ошибок от оборудования не поступало. Вывод соответствующей инфо.
+        if (!answer.matches("55 00.*")) {
+            System.out.println("\tПроверяю статус: ==> (!) (!) Что-то не так: \n\t-проверьте настройки в программе, \n\t-для надёжности получите № ФР и ФН");
+        } else {
+            System.out.println("\tПроверяю статус: ==> Ответ получен, всё исправно");
 
+//разбиваем массив, нам нужны с 0 по 4
+            String[] part = answer.split(" ");
+            int one = Integer.parseInt(part[0]);
+            int two = Integer.parseInt(part[1]);
+            int year = Integer.parseInt(part[2],16);                            // год
+            int mounth = Integer.parseInt(part[3],16);                         // мес
+            int day = Integer.parseInt(part[4],16);                           // день
+
+            System.out.println("\t===================================\n\tФН годен до: (дд/мм/гг) \n\t\t\t" + day + " : " + mounth + " : " + "20" + year);
+        }
     }
 }
