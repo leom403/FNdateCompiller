357789ce99a6e0e3f12a6121f3e40f9365e351db
VEfimov <V.Efimov**.ru>
2020-09-13 13:44:13 +0300

    moved check in the end, add LocalDate (dd-mm-yy), improve sout

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
@@ -5,25 +7,27 @@ public class Main {
         while (true) {
             System.out.println("Введите данные из строки ответ через пробел ( )");
             Scanner scanner = new Scanner(System.in);
-            String answer = scanner.nextLine();         // 55 00 15 07 02 1D 01 - 15 - 21 год
-//        String answer = "55 00 15 07 1F 1D 01";       // просто строка для проверки, что бы не вводить все время заново.
+            String answer = scanner.nextLine();
+        //String answer = "55 00 15 07 1F 1D 01";       // просто строка для проверки, что бы не вводить все время заново.
 
-//проверка на успешное чтение. 55 00 - значит, что ошибок от оборудования не поступало. Вывод соответствующей инфо.
-            if (!answer.matches("55 00.*")) {
-                System.out.println("\tПроверяю статус: ==> (!) (!) Что-то не так: \n\t-проверьте настройки в программе, \n\t-для надёжности получите № ФР и ФН");
-            } else {
-                System.out.println("\tПроверяю статус: ==> Ответ получен, всё исправно");
-
-//разбиваем массив, нам нужны с 0 по 4
+//разбиваем массив 0-1 - ответ устрйотсва. 2,3,4 - дата
                 String[] part = answer.split(" ");
                 int one = Integer.parseInt(part[0]);
                 int two = Integer.parseInt(part[1]);
                 int year = Integer.parseInt(part[2], 16);                            // год
                 int mounth = Integer.parseInt(part[3], 16);                         // мес
                 int day = Integer.parseInt(part[4], 16);                           // день
+                LocalDate localDate = LocalDate.of( day, mounth, year);
+                String formattedDate = localDate.format(DateTimeFormatter.ofPattern("yy-MM-dd"));   //почему-то нужно указывать дату зеркально :/
 
-                System.out.println("\t===================================\n\tФН годен до: (дд/мм/гг) \n\t\t\t" + day + " : " + mounth + " : " + "20" + year + "\n");
-            }
+
+//проверка на успешное чтение. 55 00 - значит, что ошибок от оборудования не поступало. Вывод соответствующей инфо.
+//Проверка перенесена дальше, что бы в дальнейшем выйти на ошибки, если будет необходимость.
+                if (one != 55 || two != 00) {
+                    System.out.println("\tПроверяю статус: ==> (!) (!) Что-то не так. Ответ от устройства: " + part[0] + " " + part[1] +  "\n\t-проверьте настройки в программе, \n\t-для надёжности получите № ФР и ФН");
+                } else {
+                System.out.println("\tПроверяю статус: ==> Ответ получен, всё исправно\n\t===================================\n\tФН годен до: " + formattedDate + "\n\t\t\t\t(дд-мм-гг)\n");
+                }
         }
     }
 }
